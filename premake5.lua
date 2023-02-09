d06cbb074b33069ffb734d5838945f5b13c163bd
Rok Oblak <oblak2003@gmail.com>
2023-02-09 20:39:09 +0100

    Added the yaml-cpp submodule

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"src/**.h",
+		"src/**.cpp",
+
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
